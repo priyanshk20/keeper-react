d80ea3c40fc9418151ea06417609caf56f540973
Priyansh Khandelwal <khandelwalpriyansh85@gmail.com>
2020-06-21 22:26:44 +0530

    adding files

---
+++ src/Components/note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note(){
+ 
+    return <div className="note">
+      <h1>This is the heading</h1>
+      <p>Thos is the body</p>
+  </div>
+};
+
+
+export default Note;
