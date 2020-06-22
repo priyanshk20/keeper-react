7a7456a82d4d6ac9718ab26021cf94403a30fa60
Priyansh Khandelwal <khandelwalpriyansh85@gmail.com>
2020-06-22 11:47:33 +0530

    updated props, arrow function and mapping

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -3,12 +3,27 @@ import ReactDom from "react-dom";
 import Header from "./header";
 import Footer from './footer';
 import Note from "./note";
+import notes from "../notes";
+
+
+
+
 function App() {
 
  return <div>
         <Header />
         <Footer />
-        <Note />
+        {notes.map((noteItem) =>
+     <Note 
+    id= {noteItem.id}
+    title= {noteItem.title}
+    content= {noteItem.content}
+    
+    />
+  )
+}
+
+    
     </div>
 }
 
--- src/Components/note.jsx
+++ src/Components/note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-function Note(){
+function Note(props){
  
     return <div className="note">
-      <h1>This is the heading</h1>
-      <p>Thos is the body</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
   </div>
 };
 
